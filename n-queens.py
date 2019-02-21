c81cd88f0a5818c5fda4b151a270ca59901d58a4
Flo Yao <floxyao@gmail.com>
2019-02-20 21:29:57 -0800

    Add files via upload

--- n-queens.py
+++ n-queens.py
@@ -8,7 +8,10 @@ Created on Sun Feb 17 18:27:41 2019
 
 import random
 import math
-import collections
+from random import randint
+from random import randrange
+import numpy
+
 
 class solveQueens:
     def __init__(self, _numQueens, _numStates):
@@ -74,87 +77,114 @@ class solveQueens:
                 queen2 = (qpos[j][0] , qpos[j][1])
                 #print(queen2)
                 if(self.collision(queen1,queen2) == False):
-                    pairs += 1;
+                    pairs += 1
             if(i==(len(positions)-1)): break
             k -= 1
         
         return pairs
-    
+        
     def geneticAlgorithm(self):
         goal = self.nCr(self.numQueens, 2)
         fitPercents = []
         allScores = []
-        
-        
+        indexes = []
         #while(True)
-        
-        
         #calculate sum of all fitness scores
         for i in range(len(self.population)):
             allScores.append(self.fitnessFunction(self.population[i]))
-        
+       
         print('allScores: ',allScores)
         total = sum(allScores)
-        
+       
         #insert list of tuples of (fitness percentage and individual)
         for j in range(len(self.population)):
-            fitScore = allScores[j]
+            fitScore = allScores[j] 
+            fitPercents.append(((fitScore / total) , self.population[j]))
+            indexes.append(i)
             
-            fitPercents.append((round((fitScore / total),2) , self.population[j]))
-            print(fitPercents[j])
-        
-        #x, y = cross_over
+        # call selection()
+        choice = self.selection(fitPercents, indexes)
 
-        #new_population = getNextPopulation();
-        
-        #self.population = new_population
-        
-        
-    
-        print("out of loop")
+        # crossover the pairs from top to bottom, if states are odd then just copy last parent to child
+        for i in range(0, len(self.population)-1, 2):
+            self.crossover(self.population[choice[i]], self.population[choice[i + 1]])  # even num of parents
+            if range(len(self.population) % 2 == 1): # odd num of parents
+                self.population[len(self.population)-1] = self.population[len(self.population)-1]
+        print("children after crossover: ", self.population)
+
+        # probability to run mutation is 1%
+        mutation_probability = 1/100.
+        for i in range(len(self.population)-1):
+            if random.random() < mutation_probability:
+                self.mutation(self.population[i])
+        print("children after mutations: ", self.population)
             
+    # ======================================================
+    # selects states based on probability
+    # returns a state
+    # input(s): list of tuple(fitScore, state)
+    # ======================================================
+    def selection(self, fitPercents, indexes):
+        choice = []
+        percents = [x[0] for x in fitPercents]
+        #print(percents)
+        # selection of pairs based on probability, has duplicate pairs
+        choice = numpy.random.choice(indexes, len(self.population), replace=True, p=percents)
+        #print("choice list of indexes based on prob: ", choice)
+        return choice
+        
+    # ======================================================
+    # crossover's two integer arrays
+    # returns both arrays after crossovers
+    # input(s): 2 arrays of integers
+    # ======================================================
+    def crossover(self, positions, positions2):
+        crosspoint = random.choice(positions)
+        positions2[crosspoint:], positions[crosspoint:] = positions[crosspoint:], positions2[crosspoint:]
+        print("---crossover--- ")
+        print("\t1st array: ", positions)
+        print("\t2nd array: ", positions2)
+        print("--------------- ")
+        return positions, positions2
+
+    # ======================================================
+    # randomly chooses an index in the array and changes
+    # its value with any number from 1-N
+    # returns the new array
+    # input(s): 2 arrays of integers
+    # ======================================================
+    def mutation(self, positions):
+        print("---mutation---")
+        print("\tarray:\t\t\t", positions)
+        rand_index = randrange(len(positions))
+        element = randint(0, self.numQueens) # may choose the same value to replace
+        positions[rand_index] = element
+        print("\tmutated array:  ", positions)
+        print("--------------- ")
+        return positions
+            
+    # ======================================================
+    # calculates the combination formula
+    # ======================================================
     def nCr(self, n,r):
         f = math.factorial
         return int(f(n) / f(r) / f(n-r))
         
-    
     #===================================================
     #detects horizontal/vertical/diagonal alignment
     #input(s): a position (x,y)
     #===================================================
     def collision(self, q1, q2):
         return (abs(q2[0]-q1[0]) == abs(q2[1]-q1[1])) or (q1[0] == q2[0]) or (q1[1] == q2[1])
-    
-#    def collision(self, x1, y1, x2, y2):
-#        print("x1 = ",x1)
-#        print("y1 = ",y1)
-#        print("x2 = ",x2)
-#        print("y2 = ",y2)
-#        print("x2-x1 = ",abs(x2-x1))
-#        print("y2-y1 = ",abs(y2-y1))
-#        return (abs(x2-x1) == abs(y2-y1)) or (x1 == x2) or (y1 == y2)
+
     
 if __name__ == '__main__':
     print("\n")
     q = solveQueens(4,4)
     
-#    population = q.getpopulation
-    
-    
-#    pos = []
-#    for i in range(4):
-#        pos.append(random.randint(0, 3))
-#        print(pos[i])
-    
-    #get population
-    
     pos = q.geneticAlgorithm();
         
     #q.show(pos)    
-    
-    #fitness = q.fitnessFunction(pos)
-    #print(fitness)
-#    q.placeQueens()
 
 
     
